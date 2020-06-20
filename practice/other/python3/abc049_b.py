427226c97be8a19c0b9df7bd28677f5e8f275bb6
shkwsk <ren7link@gmail.com>
2020-06-20 14:49:12 +0900

    abc049_b.py

---
+++ practice/other/python3/abc049_b.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+def main():
+    H,W = [int(x) for x in input().split()]
+    S = []
+    for _ in range(H):
+        S.append(input())
+    for s in S:
+        print(s)
+        print(s)
+
+if __name__ == '__main__':
+    main()
